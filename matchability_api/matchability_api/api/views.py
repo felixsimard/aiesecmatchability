9b74ffe8d7f4e46c4ca665e41b444773e7882c1a
Ali Soliman <ali.soliman95@gmail.com>
2019-08-07 16:29:02 -0400

    Create a django project to wrap the API Move all the matchability files to a subfolder "matchability_lib"

---
+++ matchability_api/matchability_api/api/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from matchability_lib.matchability import matchability
+
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'GET':
+        return Response({"message": "Hello, world!"})
+        # return Response(request.data)
+    elif request.method == 'POST':
+        return Response(matchability(request.data))
+        return Response(request.data)
