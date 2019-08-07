b243bc0b0c48de74e103fe4d07786910539b79de
Felix Simard <felixsimard@gmail.com>
2019-08-07 15:57:30 -0400

    Cleaned the repository.

--- aiesec_applications_extraction.sql
+++
@@ -1,4 +0,0 @@
-SELECT "public"."opportunity_applications".an_status      AS an_status,
-       "public"."opportunity_applications".opportunity_id AS opportunity_id
-FROM   "public"."opportunity_applications"
-LIMIT 10000000;
