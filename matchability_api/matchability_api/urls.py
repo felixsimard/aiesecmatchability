820f1cf81393d92541fe46abe1cdebf1d69fbd68
Ali Soliman <ali.soliman95@gmail.com>
2019-08-22 16:20:31 -0400

    Add psycopg2 to the requirements file.

--- matchability_api/matchability_api/urls.py
+++ matchability_api/matchability_api/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.contrib import admin
 
 from django.urls import path, include
 from rest_framework import routers
@@ -26,5 +27,5 @@ urlpatterns = [
     path('api/opportunity', opportunity_matchability),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
